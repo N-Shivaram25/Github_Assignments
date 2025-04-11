3ca6269524147eaeac780d4a3eb0935c1fffca4b
Shiva Ram <naradashishivaram25@gmail.com>
2025-04-11 12:35:26 +0530

    python calc file Added

---
+++ calc.py
@@ -0,0 +1,32 @@
+import math
+
+def get_input():
+    """Function to take input from the user."""
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    return a, b
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    
+
+def multiply(a, b):
+    
+
+def divide(a, b):
+    
+
+def square_root(a):
+    
+
+def calculator():
+    print("\nWelcome to the Mini Calculator!")
+    a, b = get_input()
+    print(f"\nYou entered: a = {a}, b = {b}")
+    print(f"1. Addition: {add(a, b)}")
+    
+
+if __name__ == "__main__":
+    calculator()
